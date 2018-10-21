95aa62d9b0bd3598691c2b90353ce1227a17df22
efvioli <efvioli@gmail.com>
2018-10-21 12:05:38 +1100

    Project set up and Failing Test

---
+++ TDDCourse/TDDCourse/Test/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDCourse.Test
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
